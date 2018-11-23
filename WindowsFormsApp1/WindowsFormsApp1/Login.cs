9bce879309468800a53fb1b67984995c56a13f52
gi <gialmeidaetec899@gmail.com>
2018-11-23 19:42:48 -0200

    inserindo comandos de login

---
+++ WindowsFormsApp1/WindowsFormsApp1/Login.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Login
+    {
+        //decalre properties 
+        public string Username { get; set; }
+        public string Userpassword { get; set; }
+
+        //intialise  
+        public Login(string caique, string zxc)
+        {
+            this.Username = caique;
+            this.Userpassword = zxc;
+        }
+        //validate string 
+        private bool StringValidator(string input)
+        {
+            string pattern = "[^a-zA-Z]";
+            if (Regex.IsMatch(input, pattern))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //validate integer 
+        private bool IntegerValidator(string input)
+        {
+            string pattern = "[^0-9]";
+            if (Regex.IsMatch(input, pattern))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //clear user inputs 
+        private void ClearTexts(string caique, string zxc)
+        {
+            caique = String.Empty;
+            zxc = String.Empty;
+        }
+        //method to check if eligible to be logged in 
+        internal bool IsLoggedIn(string caique, string zxc)
+        {
+            //check user name empty 
+            if (string.IsNullOrEmpty(caique))
+            {
+                MessageBox.Show("Digite o usuário!");
+                return false;
+
+            }
+            //check user name is valid type 
+            else if (StringValidator(caique) == true)
+            {
+                MessageBox.Show("Apenas texto é permitido");
+                ClearTexts(caique, zxc);
+                return false;
+            }
+            //check user name is correct 
+            else
+            {
+                if (Username != caique)
+                {
+                    MessageBox.Show("Usuário está incorreto.");
+                    ClearTexts(caique, zxc);
+                    return false;
+                }
+                //check password is empty 
+                else
+                {
+                    if (string.IsNullOrEmpty(zxc))
+                    {
+                        MessageBox.Show("Digite a senha.");
+                        return false;
+                    }
+                    //check password is valid 
+                    else if (IntegerValidator(zxc) == true)
+                    {
+                        MessageBox.Show("Enter only integer here");
+                        return false;
+                    }
+                    //check password is correct 
+                    else if (Userpassword != zxc)
+                    {
+                        MessageBox.Show("Senha incorreta.");
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+}
