868559aa8e17ccd6529be3269b6b1a9332b4fcce
Voxy05 <42056295+Voxy05@users.noreply.github.com>
2018-11-24 13:51:57 -0200

    Mudando tela funcionários

--- WindowsFormsApp1/WindowsFormsApp1/Properties/Resources.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Properties/Resources.Designer.cs
@@ -79,5 +79,45 @@ namespace WindowsFormsApp1.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap saveicon {
+            get {
+                object obj = ResourceManager.GetObject("saveicon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap saveicon1 {
+            get {
+                object obj = ResourceManager.GetObject("saveicon1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap saveicon2 {
+            get {
+                object obj = ResourceManager.GetObject("saveicon2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap updateicon1 {
+            get {
+                object obj = ResourceManager.GetObject("updateicon1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
